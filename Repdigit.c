eb134acdf6424d17b74190c4ce43f9a80a0d82b4
Vrfrohlich0 <129698709+Vrfrohlich0@users.noreply.github.com>
2023-04-03 04:05:30 -0300

    Add files via upload

---
+++ Repdigit.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+#include <stdlib.h>
+#include<string.h>
+
+int main (){
+
+    int i, x;
+    char str[10];
+
+
+    for(i=1000; i >= 1; i--)
+    {
+        printf("\n%d ",i);
+        itoa(i, str, 10); //To copy the valors of the int into a string
+
+        //Used to compare if the first character of the string is equal to the second and if there is a second character
+        if (str[0] == str[1] || str[1] == '\0')
+        {
+            //Used to compare if the first character of the string is equal to the third and if there is a third character
+            if(str[0] == str[2] || str[2] == '\0')
+            {
+                 printf("repdigit");
+            }
+        }
+
+    }
+
+}
